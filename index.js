6007bc067ef209157778f8b412255935ee48b3e6
tornike <berikashvilitornike@gmail.com>
2023-05-27 20:47:47 +0400

    mobile design and input validation

---
+++ index.js
@@ -0,0 +1,14 @@
+const input = document.getElementById("email-input");
+const button = document.getElementById("request");
+const error = document.getElementById("error");
+const regex =
+  /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
+button.addEventListener("click", () => {
+  if (regex.test(input.value)) {
+    input.style.border = "none";
+    error.style.display = "none";
+  } else {
+    input.style.border = "2px solid red";
+    error.style.display = "block";
+  }
+});
